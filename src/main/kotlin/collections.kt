c93f9f4bc804d5ee82e6c78bf934d34925299c6e
medicalanthrax <carlotran4@gmail.com>
2022-08-08 22:44:53 -0500

    ...

--- src/main/kotlin/collections.kt
+++ src/main/kotlin/collections.kt
@@ -1,8 +1,12 @@
 fun main() {
     val allbooks = listOf("hamlet", "romeo and juliet", "much ado about nothing")
     val library = mapOf<String, String>( "hamlet" to "shakespeare", "romeo and juliet" to "shakespeare")
-    val containshamlet = library.toList().any {it.equals("(hamlet, shakespeare)")}
+    val containshamlet = library.toList()
     println(library.toList())
     println(containshamlet)
-    //todo complete this problem
+
+    val moreBooks = mutableMapOf<String,String>("hamlet" to "shakespeare", "romeo and juliet" to "shakespeare", "harry potter" to "jk rowling")
+
+    moreBooks.getOrPut("lolita") {"weird guy"}
+    println(moreBooks)
 }
